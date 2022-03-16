3eefe0670151e1f3a5ae1b6a7100bbe5f979c831
yacine074 <benatia_yacine@hotmail.fr>
2022-03-16 14:35:50 +0100

    Update model_DL.py

--- model_DL.py
+++ model_DL.py
@@ -47,5 +47,40 @@ def get_model_DF(X_train, f_loss, f_metrics, f_activation1, f_activation2, f_act
   return model
 
 
+def get_model_CF(X_train, f_loss, f_metrics, f_activation1, f_activation2, LR) :
+    """
+
+      Structure of DL model for DF.
+
+      Parameters:
+         X_train: Channel gain array.
+         f_loss: Loss function.
+         f_metrics: List of metrics.
+         f_activation1: First activation function for the output.
+         f_activation2: Second activation function for the output.
+         LR : Learning rate.
+         
+      Returns:
+        DL model for CF.
+    """
+    opt = tf.keras.optimizers.Adam(learning_rate = LR)
+    inputs = Input(shape=(X_train.shape[1]))
+    x = Dense(128, activation='relu')(inputs)
+    x = Dense(256, activation='relu')(x)
+    x = Dense(256, activation='relu')(x)
+    x = Dense(256, activation='relu')(x)
+    output1 = Dense(1, activation=f_activation1)(x)
+    output2 = Dense(1, activation=f_activation2)(x)
+    merged = tf.keras.layers.Concatenate()([output1, output2])
+    model = Model(inputs=inputs, outputs=[merged])
+    model.compile(loss=f_loss, optimizer=opt, metrics=f_metrics)
+    model.summary()
+    return model
+
+
+
+
+
+
 
 
